f9c1efb94809dfb7e2040e47f486ffae90241c69
15bmason <blukem49@gmail.com>
2021-03-29 20:16:22 +0100

    Single message functionality

--- main.py
+++ main.py
@@ -43,5 +43,37 @@ class InstaBot:
             sleep(2)
             i += 1
 
+    def singleMessage(self, PN, PW, UN, MSG):
+        self.driver.get("https://instagram.com")
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[5]/button/span[2]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[1]/input").send_keys(PN)
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[2]/input").send_keys(PW)
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[3]/button").click()
+        sleep(12)
+        try:
+            self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
+        except:
+            sleep(12)
+            self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input").send_keys(f"{UN}")
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div/div[2]/div[1]/div/div/div").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div[1]/div/button").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea").send_keys(f"{MSG}")
+        sleep(2)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button").click()
+        sleep(2)
+           
 my_bot = InstaBot()
-my_bot.spamEddie(secret.PN, secret.PW)
+my_bot.singleMessage(secret.PN, secret.PW, "benpartridge2154", "I made an Instagram bot - Sent from the Bot")
