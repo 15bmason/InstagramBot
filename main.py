0c09ff2229d673c6aa23d82f063e5f00d4f24eef
15bmason <blukem49@gmail.com>
2021-03-29 19:50:22 +0100

    Basic Version

---
+++ main.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from time import sleep
+import secret
+
+
+class InstaBot:
+    def __init__(self, PN, PW):
+        self.driver = webdriver.Chrome("C:\\Users\\bluke\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe")
+        self.driver.get("https://instagram.com")
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[5]/button/span[2]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[1]/input").send_keys(PN)
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[2]/input").send_keys(PW)
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/form/div/div[3]/button").click()
+        sleep(12)
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input").send_keys("_richardson.edmund_")
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div/div[2]/div[1]/div/div/div").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/div[1]/div[1]/div/div[1]/div/button").click()
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea").send_keys("cringe")
+        sleep(3)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button").click()
+        sleep(10)
+
+my_bot = InstaBot(secret.PN, secret.PW)
